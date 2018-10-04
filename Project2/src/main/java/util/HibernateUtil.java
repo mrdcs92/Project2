866a9828b028e17c16e90d4829b1bf11ff334554
Dylan Smith <mrdcs92@gmail.com>
2018-10-04 09:39:53 -0400

    starting up

---
+++ Project2/src/main/java/util/HibernateUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	private static SessionFactory sessionFactory;
+	
+
+	
+	private HibernateUtil() {}
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration cfg = new Configuration();
+			sessionFactory = cfg.configure().buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+}
