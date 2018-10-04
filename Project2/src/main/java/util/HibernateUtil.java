9f02e4c8475b8de988645651e229a684a2dddce4
David Cheng <42741091+davidcheng1760@users.noreply.github.com>
2018-10-04 11:23:33 -0400

    Update HibernateUtil.java

--- Project2/src/main/java/util/HibernateUtil.java
+++ Project2/src/main/java/util/HibernateUtil.java
@@ -6,16 +6,10 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
-<<<<<<< HEAD
-	
-	private HibernateUtil() {}
-	
-=======
 
 	private HibernateUtil() {
 	}
-
->>>>>>> e37643065f21cabad3351542b86060612be16367
+	
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
 			Configuration cfg = new Configuration();
