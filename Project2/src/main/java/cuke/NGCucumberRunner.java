49ce3aa5b49d16b5fc010df03c7de7aaff99c359
anferneerod <anferneerod@outlook.com>
2018-10-08 21:54:04 -0400

    assess batch

--- Project2/src/main/java/cuke/NGCucumberRunner.java
+++ Project2/src/main/java/cuke/NGCucumberRunner.java
@@ -5,6 +5,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 //@CucumberOptions(features = {"src/test/resources/LoginSuccess.feature"}, glue = {"cuke"})
 //@CucumberOptions(features = {"src/test/resources/LoginFailure.feature"}, glue = {"cuke"})
+@CucumberOptions(features = {"src/test/resources/AssessBatchTest.feature"}, glue = {"cuke"})
 
 //@CucumberOptions(features = {"src/test/resources/NavBarHome.feature"}, glue = {"cuke"})
 //@CucumberOptions(features = {"src/test/resources/NavBarManageBatch.feature"}, glue = {"cuke"})
