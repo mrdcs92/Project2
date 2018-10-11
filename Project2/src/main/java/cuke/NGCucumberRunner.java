8dcfe3a61fa0040517d6ac073d75e228624ed458
Dylan Smith <mrdcs92@gmail.com>
2018-10-11 00:39:58 -0400

    Update NGCucumberRunner.java

--- Project2/src/main/java/cuke/NGCucumberRunner.java
+++ Project2/src/main/java/cuke/NGCucumberRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(features = {"C:\\Users\\Administrator\\.jenkins\\workspace\\Project2\\Project2\\src\\test\\resources"},
 tags = "@AssessBatch, @Footer, @LoginFailure, @LoginSuccess, @NavBarAssessBatch, @NavBarHome, @NavBarManageBatch, @NavBarPanel, @NavBarQualityAudit, @NavBarReports, @QualityAuditYear, @SettingsLocationsChange",
 glue = {"cuke"},
-plugin = {"pretty", "C:\\Users\\Administrator\\.jenkins\\workspace\\Project2\\Project2\\src\\main\\resources\\Cucumber.json"},
+plugin = {"pretty", "json:C:\\Users\\Administrator\\.jenkins\\workspace\\Project2\\Project2\\src\\main\\resources\\Cucumber.json"},
 monochrome = true)
 
 public class NGCucumberRunner extends AbstractTestNGCucumberTests {	
